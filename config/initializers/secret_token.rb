3976d141f9d6ca634d702ac258b1b1ace4490335
Daniel Escobar <danielf.5@hotmail.com>
2013-08-28 09:56:05 -0500

    primer commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Proyectoshare2::Application.config.secret_token = '5841b2b81388ae4984ef9afc2d2ff52f4379dd70cce2b732c404a676612ce1c4a947f8e3235645b54db2e8f13c5f16bf6436f6c7aebfeb85b9e0193d54729405'
